2961b1ca3dacf6e6fcc78d8fc582385ab51949d4
ntb-cicles <103062219+ntb-cicles@users.noreply.github.com>
2023-03-21 19:48:11 +0100

    Afegit echo amb coloringos

--- checkconnection.sh
+++ checkconnection.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+# Constants (codis de colors)
+Red='\033[0;31m' # Regular Red
+Green='\033[0;32m' # Regular Green
+Blue='\033[0;34m' # Regular Blue
+BRed='\033[1;31m' # Bold Red
+BGreen='\033[1;32m' # Bold Green
+BBlue='\033[1;34m' # Bold Blue
+URed='\033[4;31m' # Underline Red
+BgYellow='\033[43m' # Yellow background
+
 echo Hola mundo !!!
+
+echo -e "Hola mundo ${Red} rojo ${Green} verde y ${Blue} azul ${BgYellow} !!"
